aabda18f832a5fb408428c4886dcb626e50e335c
taiwoadebayo <mikelis135@gmail.com>
2020-05-04 18:52:12 +0100

    Setu p laravel with xampp and doing a get and post api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Studentmodel;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class ApiController extends Controller
+{
+    //
+    public function create(Request $request){
+
+        $students = new Studentmodel();
+
+        $students->firstname = $request->input('firstname');
+        $students->lastname = $request->input('lastname');
+        $students->email = $request->input('email');
+        $students->password = $request->input('password');
+
+        $students->save();
+
+        return response()->json($students);
+
+    }
+
+    public function getstudent(){
+
+        $students = [
+            "taiwo", "kehinde"
+        ];
+
+        return response()->json($students);
+
+    }
+}
