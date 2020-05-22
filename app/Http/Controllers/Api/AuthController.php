10806af849d289e51296b27e02eb8b38a85e2af2
taiwoadebayo <mikelis135@gmail.com>
2020-05-22 13:23:25 +0100

    validation to create

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+        $accessTokens = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $accessTokens]);
+    }
+
+
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+          $accessTokens = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'accessToken' => $accessTokens]);
+    }
+}
